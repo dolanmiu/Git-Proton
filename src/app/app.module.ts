6e97f0816eb3eb24ddb59cdbcbc794a84876b2ea
Dolan Miu <dolan_miu@hotmail.com>
2016-10-25 22:54:42 +0100

    added repo page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ToolbarModule } from './tool-bar';
+import { RepoManagerComponent } from './repo-manager/repo-manager.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepoManagerComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    RouterModule.forRoot([
+      { path: 'repo', component: RepoManagerComponent }
+    ]),
     ToolbarModule
   ],
   providers: [],
---
+++ src/app/repo-manager/repo-manager.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-repo-manager',
+  templateUrl: './repo-manager.component.html',
+  styleUrls: ['./repo-manager.component.scss']
+})
+export class RepoManagerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
