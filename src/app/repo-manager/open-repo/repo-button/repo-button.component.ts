88c956d724f9ca8bc704854ccf52bf746fba8068
Dolan Miu <dolan_miu@hotmail.com>
2016-11-06 00:23:14 +0000

    work on repo button

--- src/app/repo-manager/open-repo/repo-button/repo-button.component.ts
+++ src/app/repo-manager/open-repo/repo-button/repo-button.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import * as path from 'path';
 
 @Component({
-  selector: 'app-repo-button',
-  templateUrl: './repo-button.component.html',
-  styleUrls: ['./repo-button.component.scss']
+    selector: 'app-repo-button',
+    templateUrl: './repo-button.component.html',
+    styleUrls: ['./repo-button.component.scss']
 })
 export class RepoButtonComponent implements OnInit {
+    @Input() directory: string
+    projectName: string;
+    parentFolder: string;
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        if (!this.directory) {
+            console.warn('"directory" is not set as the input!');
+            return;
+        }
+        this.projectName = path.basename(this.directory);
+        this.parentFolder = path.normalize(`${this.directory}..`);
+    }
 
 }
