ac3ad9ed8a835db119ca9c5dd85d3e3cf5144dd0
Dolan Miu <dolan_miu@hotmail.com>
2016-11-13 03:27:39 +0000

    configuration saving now works!

--- main/config.ts
+++ main/config.ts
@@ -1,19 +1,41 @@
 import * as nconf from 'nconf';
 import * as path from 'path';
 import * as fs from 'fs';
+import * as mkdirp from 'mkdirp';
 
 export class Config {
 
     public writeConfig(directory: string) {
+        mkdirp(`${__dirname}/configuration`, err => {
+            if (err) {
+                return console.error(err);
+            }
+
+            fs.writeFile(`${__dirname}/configuration/projects.json`, '{}', error => {
+                if (err) {
+                    return console.log(err);
+                }
+                this.readConfig('fff');
+            });
+
+        });
+
+
+    }
+
+    private readConfig(directory: string) {
         nconf.argv().env();
-        nconf.file('projects', { file: 'configuration/projects.json' });
+        nconf.file('projects', { file: `${__dirname}/configuration/projects.json` });
 
         nconf.set(path.basename(directory), {
             directory,
         });
 
         nconf.save('projects', err => {
-            fs.readFile('configuration/projects.json', (error, data) => {
+            fs.readFile(`${__dirname}/configuration/projects.json`, (error, data) => {
+                if (error) {
+                    return console.log(error);
+                }
                 console.dir(JSON.parse(data.toString()));
             });
         });
