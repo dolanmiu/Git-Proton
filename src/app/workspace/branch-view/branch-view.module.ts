da894865f79d0115f62d1d81d9879a802d4d3ccc
Dolan <dolan_miu@hotmail.com>
2017-05-28 18:48:55 +0100

    Added semi colon

--- src/app/workspace/branch-view/branch-view.module.ts
+++ src/app/workspace/branch-view/branch-view.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { BranchViewComponent } from './branch-view.component'
+import { BranchViewComponent } from './branch-view.component';
 
 @NgModule({
     declarations: [
