043f20790fe4ceb51d703c47ae34b03be839f1d0
Dolan <dolan_miu@hotmail.com>
2017-05-28 20:20:40 +0100

    Added nodegit addGitProject

--- src/app/common/git/_real/git.service.ts
+++ src/app/common/git/_real/git.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@angular/core';
+import * as fs from 'fs';
+import * as Git from 'nodegit';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 
 @Injectable()
 export class GitService {
 
     constructor() { }
 
+    private addGitProject(directory: string): Observable<void> {
+        const subject = new Subject<void>();
+
+        fs.stat(`${directory}/.git`, (err, stats) => {
+            if (err !== null) {
+                throw new Error(`${directory} is not a Git project`);
+            }
+
+            if (!stats.isDirectory()) {
+                throw new Error(`${directory} is not a Git project`);
+            }
+
+            Git.Repository.open('test').then((repo) => {
+                subject.next();
+            });
+
+            // this.config.writeConfig(directory);
+        });
+
+        return subject.asObservable();
+    }
 }
