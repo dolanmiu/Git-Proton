9b0b1646faec5054bb83b91cddd0b9747fb0f049
Dolan <dolan_miu@hotmail.com>
2017-07-27 19:35:24 +0100

    Added Import Electron Module

--- src/app/repo-manager/open-repo/open-repo.module.ts
+++ src/app/repo-manager/open-repo/open-repo.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { ElectronModule } from 'app/common/electron/electron.module';
 import { OpenRepoRoutingModule } from './open-repo-routing.module';
 import { OpenRepoComponent } from './open-repo.component';
 import { RepoButtonComponent } from './repo-button/repo-button.component';
@@ -13,6 +14,7 @@ import { RepoButtonComponent } from './repo-button/repo-button.component';
     imports: [
         CommonModule,
         OpenRepoRoutingModule,
+        ElectronModule,
     ],
     exports: [
         OpenRepoComponent,
