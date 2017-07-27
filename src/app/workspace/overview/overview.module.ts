f4d66778374ec37c9e05bbd7e3026b580fdd6a41
Dolan <dolan_miu@hotmail.com>
2017-07-27 19:42:11 +0100

    Add temporary table

--- src/app/workspace/overview/overview.module.ts
+++ src/app/workspace/overview/overview.module.ts
@@ -1,5 +1,7 @@
+import { CdkTableModule } from '@angular/cdk';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MdInputModule, MdTableModule } from '@angular/material';
 
 import { OverviewComponent } from './overview.component';
 
@@ -9,6 +11,9 @@ import { OverviewComponent } from './overview.component';
     ],
     imports: [
         CommonModule,
+        MdInputModule,
+        MdTableModule,
+        CdkTableModule,
     ],
     exports: [
         OverviewComponent,
