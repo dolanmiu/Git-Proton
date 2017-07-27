5e9c7e664be36be920f7144c1308325403976c36
Dolan <dolan_miu@hotmail.com>
2017-07-27 19:37:04 +0100

    Add stub

--- src/app/common/git/git.service.ts
+++ src/app/common/git/git.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 
 @Injectable()
 export class GitService {
 
     constructor() { }
 
+    public addGitProject(directory: string): Observable<void> {
+
+        return Observable.empty();
+    }
 }
