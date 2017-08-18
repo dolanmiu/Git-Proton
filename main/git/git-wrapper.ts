c6fd12da93c7ec307645ca05fb24ddee30a888d3
Dolan <dolan_miu@hotmail.com>
2017-08-18 02:38:30 +0100

    Tslint fix

--- main/git/git-wrapper.ts
+++ main/git/git-wrapper.ts
@@ -8,18 +8,18 @@ export class GitWrapper {
         return new Promise<void>((resolve, reject) => {
             Git.Repository.open(repoName)
                 // Open the master branch.
-                .then(function (repo) {
+                .then((repo) => {
                     console.log(repo);
                     return repo.getMasterCommit();
                 })
                 // Display information about commits on master.
-                .then(function (firstCommitOnMaster) {
+                .then((firstCommitOnMaster) => {
                     // Create a new history event emitter.
-                    let history = firstCommitOnMaster.history();
+                    const history = firstCommitOnMaster.history();
                     // Create a counter to only show up to 9 entries.
                     let count = 0;
                     // Listen for commit events from the history.
-                    history.on('commit', function (commit) {
+                    history.on('commit', (commit) => {
                         // Disregard commits past 9.
                         if (++count >= 9) {
                             return;
@@ -27,7 +27,7 @@ export class GitWrapper {
                         // Show the commit sha.
                         console.log('commit ' + commit.sha());
                         // Store the author object.
-                        let author = commit.author();
+                        const author = commit.author();
                         // Display author information.
                         console.log('Author:\t' + author.name() + ' <' + author.email() + '>');
                         // Show the commit date.
