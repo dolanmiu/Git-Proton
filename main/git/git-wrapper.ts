390c12e8da528ae91397e37e99126f51eb3db4de
Dolan <dolan_miu@hotmail.com>
2017-08-22 00:11:27 +0100

    Make commits into an array

--- main/git/git-wrapper.ts
+++ main/git/git-wrapper.ts
@@ -1,4 +1,3 @@
-import * as Git from 'nodegit';
 import { Observable } from 'rxjs/Rx';
 import * as shell from 'shelljs';
 import * as which from 'which';
@@ -25,24 +24,27 @@ export class GitWrapper {
 
     constructor() {
         const resolved = which.sync('node');
-        console.log(resolved);
         shell.config.execPath = resolved; // Replace this with the real path
 
     }
 
-    public openRepo(repoName: string): Observable<any> {
+    public openRepo(repoName: string): Observable<CommitModel[]> {
         const commit$ = new Observable<string>((observer) => {
             shell.exec(`cd ${repoName} && git log --parents --format=fuller`, (code, stdout, stderr) => {
                 observer.next(stdout);
                 observer.complete();
             });
         }).flatMap((stdout) => {
-            const res = stdout.split('\ncommit');
+            const res = stdout.split('\ncommit').map((commit) => {
+                return `commit${commit}`;
+            });
             return res;
         }).map((raw) => {
-            const matches = raw.match(/commit(.+)\nAuthor:(.+)<(.+)>\nAuthorDate:(.+)\nCommit:(.+)\nCommitDate:(.+)\n\n([\s\S]+)/g);
-            const currentSha = matches[1].trim();
-            const parentShas = currentSha.trim().split(' ');
+            const pattern = /commit(.+)\nAuthor:(.+)<(.+)>\nAuthorDate:(.+)\nCommit:(.+)<(.+)>\nCommitDate:(.+)\n\n([\s\S]+)/g;
+            const matches = pattern.exec(raw);
+            const shas = matches[1].trim().split(' ');
+            const currentSha = shas[0];
+            const parentShas = [];
 
             return {
                 author: {
@@ -61,49 +63,49 @@ export class GitWrapper {
                 },
                 message: matches[8].trim(),
             } as CommitModel;
-        });
+        }).toArray();
 
         return commit$;
     }
 
-    public openRepo2(repoName: string): Observable<CommitModel[]> {
-        const commit$ = new Observable<CommitModel[]>((observer) => {
-            Git.Repository.open(repoName)
-                // Open the master branch.
-                .then((repo) => {
-                    console.log(repo);
-                    return repo.getMasterCommit();
-                })
-                .then((firstCommitOnMaster) => {
-                    const history: any = firstCommitOnMaster.history();
+    // public openRepo2(repoName: string): Observable<CommitModel[]> {
+    //     const commit$ = new Observable<CommitModel[]>((observer) => {
+    //         Git.Repository.open(repoName)
+    //             // Open the master branch.
+    //             .then((repo) => {
+    //                 console.log(repo);
+    //                 return repo.getMasterCommit();
+    //             })
+    //             .then((firstCommitOnMaster) => {
+    //                 const history: any = firstCommitOnMaster.history();
 
-                    history.on('end', (commits: any[]) => {
-                        const data = commits.map<CommitModel>((commit) => {
-                            return {
-                                author: {
-                                    name: commit.author().name(),
-                                    email: commit.author().email(),
-                                },
-                                sha: commit.sha(),
-                                message: commit.message(),
-                                date: commit.date(),
-                            };
-                        });
-                        for (const commit of commits) {
-                            console.log('commit ' + commit.sha());
-                            const author = commit.author();
-                            console.log('Author:\t' + author.name() + ' <' + author.email() + '>');
-                            console.log('Date:\t' + commit.date());
-                            console.log('\n    ' + commit.message());
-                        }
-                        observer.next(data);
-                        observer.complete();
-                    });
+    //                 history.on('end', (commits: any[]) => {
+    //                     const data = commits.map<CommitModel>((commit) => {
+    //                         return {
+    //                             author: {
+    //                                 name: commit.author().name(),
+    //                                 email: commit.author().email(),
+    //                             },
+    //                             sha: commit.sha(),
+    //                             message: commit.message(),
+    //                             date: commit.date(),
+    //                         };
+    //                     });
+    //                     for (const commit of commits) {
+    //                         console.log('commit ' + commit.sha());
+    //                         const author = commit.author();
+    //                         console.log('Author:\t' + author.name() + ' <' + author.email() + '>');
+    //                         console.log('Date:\t' + commit.date());
+    //                         console.log('\n    ' + commit.message());
+    //                     }
+    //                     observer.next(data);
+    //                     observer.complete();
+    //                 });
 
-                    history.start();
-                });
-        });
+    //                 history.start();
+    //             });
+    //     });
 
-        return commit$;
-    }
+    //     return commit$;
+    // }
 }
