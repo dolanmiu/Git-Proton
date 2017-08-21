5036680cd115b3a2dc4fcb0440de421f3fdf8f65
Dolan <dolan_miu@hotmail.com>
2017-08-21 22:34:02 +0100

    Add transformations to git command

--- main/git/git-wrapper.ts
+++ main/git/git-wrapper.ts
@@ -1,19 +1,72 @@
 import * as Git from 'nodegit';
 import { Observable } from 'rxjs/Rx';
+import * as shell from 'shelljs';
+import * as which from 'which';
 
 interface CommitModel {
     author: {
         name: string,
         email: string,
+        date: Date,
     };
-    sha: string;
+    commit: {
+        name: string,
+        email: string,
+        date: Date,
+    };
+    sha: {
+        current: string,
+        parents: string[],
+    };
     message: string;
-    date: Date;
 }
 
 export class GitWrapper {
 
-    public openRepo(repoName: string): Observable<CommitModel[]> {
+    constructor() {
+        const resolved = which.sync('node');
+        console.log(resolved);
+        shell.config.execPath = resolved; // Replace this with the real path
+
+    }
+
+    public openRepo(repoName: string): Observable<any> {
+        const commit$ = new Observable<string>((observer) => {
+            shell.exec(`cd ${repoName} && git log --parents --format=fuller`, (code, stdout, stderr) => {
+                observer.next(stdout);
+                observer.complete();
+            });
+        }).flatMap((stdout) => {
+            const res = stdout.split('\ncommit');
+            return res;
+        }).map((raw) => {
+            const matches = raw.match(/commit(.+)\nAuthor:(.+)<(.+)>\nAuthorDate:(.+)\nCommit:(.+)\nCommitDate:(.+)\n\n([\s\S]+)/g);
+            const currentSha = matches[1].trim();
+            const parentShas = currentSha.trim().split(' ');
+
+            return {
+                author: {
+                    name: matches[2].trim(),
+                    email: matches[3].trim(),
+                    date: new Date(matches[4]),
+                },
+                commit: {
+                    name: matches[5].trim(),
+                    email: matches[6].trim(),
+                    date: new Date(matches[7]),
+                },
+                sha: {
+                    current: currentSha,
+                    parents: parentShas,
+                },
+                message: matches[8].trim(),
+            } as CommitModel;
+        });
+
+        return commit$;
+    }
+
+    public openRepo2(repoName: string): Observable<CommitModel[]> {
         const commit$ = new Observable<CommitModel[]>((observer) => {
             Git.Repository.open(repoName)
                 // Open the master branch.
