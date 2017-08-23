24765f0ef5f0e8b5af43aeaf7171103e1a9669fa
Dolan <dolan_miu@hotmail.com>
2017-08-24 00:14:46 +0100

    Use ng-cli-electron sample data

--- src/app/common/git/git.service.ts
+++ src/app/common/git/git.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
 
-import { SAMPLE_DATA } from './sample-data';
+import { NG_CLI_ELECTRON } from './sample-data';
 import { CommitModel } from './tree/commit-model';
 import { CommitModelFactoryService } from './tree/commit-model-factory.service';
 import { TreeGeneratorService } from './tree/tree-generator.service';
@@ -12,7 +12,7 @@ export class GitService {
     constructor(private treeBuilder: TreeGeneratorService, private modelFactory: CommitModelFactoryService) { }
 
     public addGitProject(directory: string): Observable<CommitModel[]> {
-        const commitModel = this.modelFactory.create(SAMPLE_DATA);
+        const commitModel = this.modelFactory.create(NG_CLI_ELECTRON);
         this.treeBuilder.createTree(commitModel);
         return Observable.of(commitModel);
     }
