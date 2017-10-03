7851faa164120f881ffa95ea50456032680e7279
Dolan <dolan_miu@hotmail.com>
2017-10-03 02:17:05 +0100

    Add initial direction to node

--- src/app/common/git/tree/path-finding/nodes/commit-node.ts
+++ src/app/common/git/tree/path-finding/nodes/commit-node.ts
@@ -1,6 +1,10 @@
 import { Path } from '../path';
 import { Node } from './node';
 
+export const enum NodeDirection {
+    NODE_HORIZONTAL = 1, NODE_VERTICAL = 2, NODE_LEFT = 3, NODE_RIGHT = 4, NODE_BOTTOM = 5,
+}
+
 export class DataNode<T> extends Node {
 
     constructor(path: Path, public readonly commit: T) {
@@ -9,5 +13,33 @@ export class DataNode<T> extends Node {
 
     public toString(): string {
         return 'o';
+    }
+
+    public get Direction(): NodeDirection {
+        // const currentPosition = this.path.getCoordinates(this);
+        const neighbours = this.path.findNeighbouringNodes(this);
+
+        if (neighbours.next === undefined) {
+            return NodeDirection.NODE_VERTICAL;
+        }
+
+        // let previousDelta: Vector;
+
+        // if (neighbours.previous === undefined) {
+        //     previousDelta = {
+        //         x: this.path.PreviousDestination.x - currentPosition.x,
+        //         y: this.path.PreviousDestination.y - currentPosition.y,
+        //     };
+        // } else {
+        //     previousDelta = {
+        //         x: neighbours.previous.position.x - currentPosition.x,
+        //         y: neighbours.previous.position.y - currentPosition.y,
+        //     };
+        // }
+
+        // const nextDelta: Vector = {
+        //     x: neighbours.next.position.x - currentPosition.x,
+        //     y: neighbours.next.position.y - currentPosition.y,
+        // };
     }
 }
