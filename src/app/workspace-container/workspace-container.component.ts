f1836099dd94ceaa89fc813f2cb1ae1874242a62
Dolan <dolan_miu@hotmail.com>
2018-02-23 18:54:20 +0000

    Progress on tab calculation

--- src/app/workspace-container/workspace-container.component.ts
+++ src/app/workspace-container/workspace-container.component.ts
@@ -1,6 +1,12 @@
 import { animate, group, query, style, transition, trigger } from '@angular/animations';
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+interface Tab {
+    link: string;
+    name: string;
+}
 
 @Component({
     selector: 'app-workspace-container',
@@ -23,11 +29,69 @@ import { RouterOutlet } from '@angular/router';
                     ),
                 ]),
             ]),
+
+            transition('left => *', [
+                query(':enter, :leave', style({ position: 'fixed', width: '100%' }), { optional: true }),
+                group([
+                    query(
+                        ':enter',
+                        [style({ transform: 'translateX(-100%)' }), animate('.6s ease-out', style({ transform: 'translateX(0%)' }))],
+                        { optional: true },
+                    ),
+                    query(
+                        ':leave',
+                        [style({ transform: 'translateX(0%)' }), animate('.6s ease-out', style({ transform: 'translateX(100%)' }))],
+                        { optional: true },
+                    ),
+                ]),
+            ]),
         ]),
     ],
 })
 export class WorkspaceContainerComponent {
-    public getPage(outlet: RouterOutlet): object {
-        return outlet.activatedRouteData.page;
+    public tabs: Tab[] = [
+        {
+            name: 'hey',
+            link: 'a',
+        },
+        {
+            name: 'sister',
+            link: 'b',
+        },
+        {
+            name: 'soul',
+            link: 'c',
+        },
+    ];
+    @ViewChild('appOutlet') public appOutlet: RouterOutlet;
+    public selectedTab: Tab;
+    public currentTab: Tab;
+    public pageState: string;
+
+    constructor(private location: Location) {}
+
+    public getPage(outlet: RouterOutlet): void {
+        const selectedTabIndex = this.tabs.indexOf(this.selectedTab);
+        const workspaceName = this.location
+            .path()
+            .split('/')
+            .slice(-1)[0];
+        console.log(this.currentTab);
+        const currentTabIndex = this.tabs.indexOf(this.currentTab);
+
+        this.currentTab = this.tabs.find((x) => {
+            return x.link === workspaceName;
+        });
+
+        if (selectedTabIndex <= currentTabIndex) {
+            this.pageState = 'left';
+        } else {
+            this.pageState = 'right';
+        }
+    }
+
+    public switchTab(tab: Tab): void {
+        this.selectedTab = tab;
+        this.getPage(this.appOutlet);
     }
 }
