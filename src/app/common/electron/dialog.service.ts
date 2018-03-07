d8bc7d8df387ccfc1b5aee65ed0b7e1fe166d8aa
Dolan <dolan_miu@hotmail.com>
2018-03-07 19:32:28 +0000

    Open dialog with multiple projects

--- src/app/common/electron/dialog.service.ts
+++ src/app/common/electron/dialog.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
+const DIALOGS = [{ path: '', name: 'First Project' }, { path: '', name: 'Second Project' }, { path: '', name: 'Third Project' }];
+
 @Injectable()
 export class DialogService {
-    constructor() {}
+    private accessCounter: number;
+
+    constructor() {
+        this.accessCounter = 0;
+    }
 
     public openDialog(): Observable<{ path: string; name: string }> {
         console.log('Pretending to open dialog');
-        return Observable.of({ path: '', name: 'Fake Project' });
+        const observable$ = Observable.of(DIALOGS[this.accessCounter]);
+        this.accessCounter++;
+
+        return observable$;
     }
 }
