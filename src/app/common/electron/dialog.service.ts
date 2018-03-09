4363653609a15d5ab8d9079a753e17b466ebcb1a
Dolan <dolan_miu@hotmail.com>
2018-03-09 01:26:17 +0000

    Add localhost serving for electron

--- src/app/common/electron/dialog.service.ts
+++ src/app/common/electron/dialog.service.ts
@@ -1,21 +1,55 @@
 import { Injectable } from '@angular/core';
+import { remote } from 'electron';
+import * as path from 'path';
 import { Observable } from 'rxjs/Observable';
 
-const DIALOGS = [{ path: '', name: 'First Project' }, { path: '', name: 'Second Project' }, { path: '', name: 'Third Project' }];
+const FAKE_DIALOGS = [{ path: '', name: 'First Project' }, { path: '', name: 'Second Project' }, { path: '', name: 'Third Project' }];
 
 @Injectable()
 export class DialogService {
+    public remote: typeof remote;
     private accessCounter: number;
 
     constructor() {
+        if (this.isElectron()) {
+            this.remote = window.require('electron').remote;
+        }
         this.accessCounter = 0;
     }
 
-    public openDialog(): Observable<{ path: string; name: string }> {
+    public openDialogElectron(): Observable<{ path: string; name: string }> {
+        const openDialog = Observable.bindCallback(this.remote.dialog.showOpenDialog);
+        return openDialog({
+            properties: ['openDirectory'],
+        }).switchMap((directories) => {
+            if (!directories || directories.length === 0) {
+                return Observable.empty();
+            }
+
+            const fullPath = directories[0];
+            const projectName = path.basename(fullPath);
+
+            return Observable.of({ path: fullPath, name: projectName });
+        });
+    }
+
+    public openDialogWeb(): Observable<{ path: string; name: string }> {
         console.log('Pretending to open dialog');
-        const observable$ = Observable.of(DIALOGS[this.accessCounter]);
+        const observable$ = Observable.of(FAKE_DIALOGS[this.accessCounter]);
         this.accessCounter++;
 
         return observable$;
+    }
+
+    public openDialog(): Observable<{ path: string; name: string }> {
+        if (this.isElectron()) {
+            return this.openDialogElectron();
+        } else {
+            return this.openDialogWeb();
+        }
+    }
+
+    private isElectron(): void {
+        return window && window.process && window.process.type;
     }
 }
