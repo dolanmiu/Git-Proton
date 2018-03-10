4ccb7c4e7591b3718ad04eb5e4e6b6ee555bf937
Dolan Miu <dolan_miu@hotmail.com>
2018-03-10 19:27:03 +0000

    Fix tests

--- src/app/common/git/git.service.spec.ts
+++ src/app/common/git/git.service.spec.ts
@@ -1,4 +1,5 @@
 import { inject, TestBed } from '@angular/core/testing';
+import { StoreModule } from '@ngrx/store';
 
 import { GitService } from './git.service';
 import { TreeModule } from './tree/tree.module';
@@ -7,7 +8,7 @@ describe('GitService', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
             providers: [GitService],
-            imports: [TreeModule],
+            imports: [TreeModule, StoreModule.forRoot({})],
         });
     });
 
