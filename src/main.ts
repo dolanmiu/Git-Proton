0399e0f0955371715ff2f878d47aeed3eaaebf57
Dolan <dolan_miu@hotmail.com>
2018-03-15 02:58:59 +0000

    Add observable methods

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,10 @@ import { environment } from './environments/environment';
 
 import 'rxjs/add/observable/bindCallback';
 import 'rxjs/add/observable/empty';
+import 'rxjs/add/observable/interval';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/find';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/switchMap';
--- src/test.ts
+++ src/test.ts
@@ -12,8 +12,10 @@ import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@ang
 
 import 'rxjs/add/observable/bindCallback';
 import 'rxjs/add/observable/empty';
+import 'rxjs/add/observable/interval';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/find';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/switchMap';
