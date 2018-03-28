0943dde6a506d1572cb4f2a3e4cd6d2b0ba81d4e
Dolan <dolan_miu@hotmail.com>
2018-03-29 00:17:45 +0100

    Order imports

--- src/app/workspace-container/tab-bar/tab-bar.module.ts
+++ src/app/workspace-container/tab-bar/tab-bar.module.ts
@@ -2,10 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { TabBarComponent } from './tab-bar.component';
-import { TabComponent } from './tab/tab.component';
 import { CloseButtonComponent } from './close-button/close-button.component';
 import { NewTabComponent } from './new-tab/new-tab.component';
+import { TabBarComponent } from './tab-bar.component';
+import { TabComponent } from './tab/tab.component';
 
 @NgModule({
     imports: [CommonModule, RouterModule],
