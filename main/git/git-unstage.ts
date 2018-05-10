378b818359d8291193a3aa61ef4c4f127fa6511f
Dolan <dolan_miu@hotmail.com>
2018-05-10 01:40:11 +0100

    Add unstaging/reset ability

--- main/git/git-unstage.ts
+++ main/git/git-unstage.ts
@@ -1,24 +1,10 @@
 import * as nodegit from 'nodegit';
+// Reset.reset resets HARD/SOFT
+// Index.remove deletes from index, weird behavior
 
 // This function removes files to staging area (index)
-export default function unstage(directory: string, filePaths: string[], fn: (data: StatusData[]) => void): void {
-    let index;
-
-    nodegit.Repository.open(directory)
-        .then((repo) => {
-            return repo.refreshIndex();
-        })
-        .then((indexResult) => {
-            index = indexResult;
-        })
-        .then(() => {
-            return index.removeFall(filePaths);
-        })
-        .then(() => {
-            return index.write();
-        })
-        .then(() => {
-            console.log('written to index');
-            return index.writeTree();
-        });
+export default async function unstage(directory: string, filePaths: string[], fn: (data: StatusData[]) => void): Promise<void> {
+    const repo = await nodegit.Repository.open(directory);
+    const headCommit = await repo.getHeadCommit();
+    return nodegit.Reset.default(repo, headCommit, filePaths);
 }
