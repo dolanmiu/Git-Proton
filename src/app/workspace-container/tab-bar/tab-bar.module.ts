92608e57026ce50584af178e8d70e77dbbffee7e
Dolan <dolan_miu@hotmail.com>
2018-10-20 16:45:59 +0100

    Remove angular font-awesome

--- src/app/workspace-container/tab-bar/tab-bar.module.ts
+++ src/app/workspace-container/tab-bar/tab-bar.module.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { CloseButtonComponent } from './close-button/close-button.component';
 import { NewTabComponent } from './new-tab/new-tab.component';
@@ -9,7 +8,7 @@ import { TabBarComponent } from './tab-bar.component';
 import { TabComponent } from './tab/tab.component';
 
 @NgModule({
-    imports: [CommonModule, FontAwesomeModule, RouterModule],
+    imports: [CommonModule, RouterModule],
     declarations: [TabBarComponent, TabComponent, CloseButtonComponent, NewTabComponent],
     exports: [TabBarComponent],
 })
--- src/main.ts
+++ src/main.ts
@@ -17,12 +17,6 @@ import 'rxjs/add/operator/take';
 import 'rxjs/add/operator/toArray';
 import 'rxjs/add/operator/zip';
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fal } from '@fortawesome/pro-light-svg-icons';
-
-// Add an icon to the library for convenient access in other components
-library.add(fal);
-
 if (environment.production) {
     enableProdMode();
 }
