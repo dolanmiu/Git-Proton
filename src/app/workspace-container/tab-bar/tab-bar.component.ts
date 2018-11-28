e0fa7ecc15c23a56d5199546d3ef9f00e8656ba2
Dolan <dolan_miu@hotmail.com>
2018-11-28 03:11:24 +0000

    Clean unused left-right swiping on tabs

--- src/app/workspace-container/tab-bar/tab-bar.component.ts
+++ src/app/workspace-container/tab-bar/tab-bar.component.ts
@@ -1,7 +1,4 @@
-// tslint:disable:no-object-mutation
-// tslint:disable:readonly-keyword
-import { Location } from '@angular/common';
-import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 
@@ -20,12 +17,8 @@ interface Tab {
 })
 export class TabBarComponent {
     public readonly tabs$: Observable<Tab[]>;
-    public selectedTab: Tab;
-    public currentTab: Tab;
-    public pageState: string;
-    @Output() public readonly pageStateChanged: EventEmitter<{}> = new EventEmitter();
 
-    constructor(private readonly location: Location, readonly store: Store<AppState>) {
+    constructor(readonly store: Store<AppState>) {
         this.tabs$ = store.select(getProjectsArray).map((projects) => {
             return projects.map((project) => {
                 return {
@@ -34,33 +27,5 @@ export class TabBarComponent {
                 };
             });
         });
-    }
-
-    public switchTab(tab: Tab): void {
-        this.selectedTab = tab;
-        this.tabs$
-            .do((tabs) => {
-                const selectedTabIndex = tabs.indexOf(this.selectedTab);
-                const workspaceName = this.location
-                    .path()
-                    .split('/')
-                    .slice(-1)[0];
-
-                this.currentTab = tabs.find((x) => x.link === workspaceName);
-                const currentTabIndex = tabs.indexOf(this.currentTab);
-
-                if (selectedTabIndex <= currentTabIndex) {
-                    this.pageState = this.pageState === 'left' ? 'left1' : 'left';
-                } else {
-                    this.pageState = this.pageState === 'right' ? 'right1' : 'right';
-                }
-                this.pageStateChanged.emit(this.pageState);
-            })
-            .subscribe();
-    }
-
-    public switchToAdd(): void {
-        this.pageState = this.pageState === 'right' ? 'right1' : 'right';
-        this.pageStateChanged.emit(this.pageState);
     }
 }
