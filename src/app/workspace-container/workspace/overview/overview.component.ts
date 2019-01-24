4e7056bd1133e57dfa600244100ec26ff5da0bd2
Dolan <dolan_miu@hotmail.com>
2019-01-24 00:38:26 +0000

    Work on gitgraph implementation

--- src/app/workspace-container/workspace/overview/overview.component.ts
+++ src/app/workspace-container/workspace/overview/overview.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { GitgraphCore } from 'gitgraph-core';
 import { Observable } from 'rxjs';
+
+import { getCurrentProject } from 'app/store';
 
 @Component({
     selector: 'app-overview',
@@ -9,10 +12,42 @@ import { Observable } from 'rxjs';
 })
 export class OverviewComponent implements OnInit {
     public readonly tree$: Observable<TreeState>;
+    public readonly core: GitgraphCore;
+
+    public readonly allCommits$: Observable<GitCommitModel>;
 
     constructor(readonly store: Store<AppState>) {
         this.tree$ = store.select('tree');
+
+        this.core = new GitgraphCore();
+        this.core.template.arrow.size = 1;
+        this.core.template.arrow.color = '000';
+
+        this.allCommits$ = store
+            .select(getCurrentProject)
+            .map((project) => (project ? project.commits : []))
+            .flatMap((commits) => commits);
     }
 
-    public ngOnInit(): void {}
+    public ngOnInit(): void {
+        setTimeout(() => {
+            this.commit();
+        }, 1000);
+        const master = this.core.branch('master');
+
+        this.allCommits$.subscribe((commits) => {
+            master.commit(commits.message);
+            console.log(commits);
+        });
+    }
+
+    public commit(): void {
+        // const master = this.core.branch('master').commit('Initial commit');
+        // const develop = this.core.branch('develop');
+        // develop.commit('one');
+        // master.commit('two');
+        // develop.commit('three');
+        // master.merge(develop);
+        // master.commit();
+    }
 }
--- src/app/workspace-container/workspace/overview/overview.module.ts
+++ src/app/workspace-container/workspace/overview/overview.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { GitgraphAngularModule } from 'gitgraph-angular';
 
 import { OverviewComponent } from './overview.component';
 import { TableModule } from './table/table.module';
 
 @NgModule({
     declarations: [OverviewComponent],
-    imports: [CommonModule, TableModule],
+    imports: [CommonModule, TableModule, GitgraphAngularModule],
     exports: [OverviewComponent],
 })
 export class OverviewModule {}
